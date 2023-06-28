abef59bc1812c0fa7b28798edb87cac61f65bf5a
Mandar Sawant <66461637+MandarSawant18@users.noreply.github.com>
2023-06-28 20:12:30 +0530

    Add files via upload

--- Data with Danny SQL Challenge/Case Study 3 - Foodie-fi/Solution files sql/Section-B.sql
+++ Data with Danny SQL Challenge/Case Study 3 - Foodie-fi/Solution files sql/Section-B.sql
@@ -132,109 +132,80 @@
 
 
 
+-- 9.	How many days on average does it take for a customer to an annual plan from the day they join Foodie-Fi?
 
+    -- Steps-  Find start_date of customers with trial plan
+            -- Find date of customers when they start annual plan
+            -- Subtract both the dates and take avg
 
+        with table1 AS 
+            (SELECT customer_id,
+                    start_date AS trial_date
+            FROM subscriptions
+            WHERE plan_id = 0), 
 
+        table2 AS 
+            (SELECT customer_id,
+                    start_date AS annual_plan_date
+            FROM subscriptions
+            WHERE plan_id = 3)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-9.	How many days on average does it take for a customer to an annual plan from the day they join Foodie-Fi?
-
-with table1 AS 
-	(SELECT customer_id,
-		 start_date AS trial_date
-	FROM subscriptions
-	WHERE plan_id = 0), 
-table2 AS 
-	(SELECT customer_id,
-		 start_date AS annual_plan_date
-	FROM subscriptions
-	WHERE plan_id = 3)
-
-SELECT round(avg(t2.annual_plan_date - t1.trial_date)) AS avg_no_of_days
-FROM table1 t1
-JOIN table2 t2
-	ON t1.customer_id = t2.customer_id
+        SELECT round(avg(t2.annual_plan_date - t1.trial_date)) AS avg_no_of_days
+        FROM table1 t1
+        JOIN table2 t2
+            ON t1.customer_id = t2.customer_id
 
  
-It takes an average 105 days for customers to upgrade to an annual plan after they join Foodie-Fi.
-
-Steps- Find start_date of customers with trial plan
-Find date of customers when they start annual plan
-Subtract both the dates and take avg
+-- It takes an average 105 days for customers to upgrade to an annual plan after they join Foodie-Fi.
 
 
 
+-- 10.	Can you further breakdown this average value into 30 day periods (i.e. 0-30 days, 31-60 days etc)
 
+    -- Steps-  Find start_date of customers with trial plan
+            -- Find date of customers when they start annual plan
+            -- width_bucket(operand, min, max, num_of_buckets)
+            -- width_bucket(diff in dates, 0, 360,12)
+            -- Then count these bucket numbers to get the customer count in that bracket
 
+        with table1 AS 
+            (SELECT customer_id,
+                    start_date AS trial_date
+            FROM subscriptions
+            WHERE plan_id = 0),
 
-10.	Can you further breakdown this average value into 30 day periods (i.e. 0-30 days, 31-60 days etc)
+        table2 AS 
+            (SELECT customer_id,
+                    start_date AS annual_plan_date
+            FROM subscriptions
+            WHERE plan_id = 3),
 
-with table1 AS 
-	(SELECT customer_id,
-		 start_date AS trial_date
-	FROM subscriptions
-	WHERE plan_id = 0), 
-table2 AS 
-	(SELECT customer_id,
-		 start_date AS annual_plan_date
-	FROM subscriptions
-	WHERE plan_id = 3), 
-day_bins AS 
-	(SELECT width_bucket(t2.annual_plan_date - t1.trial_date,
-		0,
-		360,
-		12) AS bracket_no,
-		 round((annual_plan_date - trial_date)) AS no_of_days
-	FROM table1 t1
-	JOIN table2 t2
-		ON t1.customer_id = t2.customer_id)
+        day_bins AS 
+            (SELECT width_bucket(t2.annual_plan_date - t1.trial_date,0,360,12) AS bracket_no,
+                    round((annual_plan_date - trial_date)) AS no_of_days
+            FROM table1 t1
+            JOIN table2 t2
+                ON t1.customer_id = t2.customer_id)
 
-SELECT concat(((bracket_no-1)*30 +1),'-',bracket_no*30,' days') AS day_bracket, 
-  count(bracket_no)::integer AS customer_count
-FROM day_bins
-GROUP BY  bracket_no
-ORDER BY  bracket_no
-
- 
-Steps- Find start_date of customers with trial plan
-Find date of customers when they start annual plan
-width_bucket(operand, min, max, num_of_buckets)
-width_bucket(diff in dates, 0, 360,12)
-Then count these bucket numbers to get the customer count in that bracket
-
-
-
-
-
-
-
-
-11.	How many customers downgraded from a pro monthly to a basic monthly plan in 2020?
-
-WITH table1 AS 
-	(SELECT *,
-		 Lead(plan_id) over( PARTITION BY customer_id
-	ORDER BY  start_date) AS next_plan
-	FROM subscriptions
-	WHERE Extract(year
-	FROM start_date) = '2020')
-
-SELECT Count(*) AS downgraded_customers
-FROM table1
-WHERE plan_id = 2 AND next_plan = 1
+        SELECT  concat(((bracket_no-1)*30 +1),'-',bracket_no*30,' days') AS day_bracket, 
+                count(bracket_no)::integer AS customer_count
+        FROM day_bins
+        GROUP BY  bracket_no
+        ORDER BY  bracket_no
 
  
 
-In the year 2020, no customer downgraded from pro monthly to basic monthly. Pro monthly plan must have been good value for money.
+
+-- 11.	How many customers downgraded from a pro monthly to a basic monthly plan in 2020?
+
+        WITH table1 AS 
+            (SELECT *,
+                    Lead(plan_id) over( PARTITION BY customer_id ORDER BY  start_date) AS next_plan
+            FROM subscriptions
+            WHERE Extract(year FROM start_date) = '2020')
+
+        SELECT Count(*) AS downgraded_customers
+        FROM table1
+        WHERE plan_id = 2 AND next_plan = 1
+
+-- In the year 2020, no customer downgraded from pro monthly to basic monthly. Pro monthly plan must have been good value for money.
